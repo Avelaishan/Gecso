35d891f7a062b92fd68d162b3d8c0ad6a741917e
Avel <54955829+Avelaishan@users.noreply.github.com>
2021-09-07 14:49:49 +0300

    GameMechanic10%/100%
    Grid generator ready, time to think how implement game mechanic

--- Assets/Scripts/HexGrid/HexGridGenerator.cs
+++ Assets/Scripts/HexGrid/HexGridGenerator.cs
@@ -6,19 +6,49 @@ public class HexGridGenerator : MonoBehaviour
 {
     public GameObject hexPrefab;
     public Transform holder;
-    [SerializeField] int mapWiedth = 10;
-    [SerializeField] int mapHeight = 10;
+    [SerializeField] int mapWiedth = 6;
+    [SerializeField] int mapHeight = 3;
 
     float titleXOffset = 1.8f;
     float titleYOffset = 1.6f;
 
-    List<GameObject> HexList;
+    public List<GameObject> tiles = new List<GameObject>();
 
     void Start()
     {
         CreateHexTileMap();
+        RandomNumberGenerator();
     }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Vector3 hexPosition = hit.transform.gameObject.transform.position;
+                for(int i = 0; i<tiles.Count; i++)
+                {
+                    if (tiles[i].transform.position == hexPosition)
+                    {
+                        Debug.Log(tiles[i].name);
+                    }
+                }
+                if (hit.collider.gameObject == gameObject)
+                {
+                    if (hit.collider.tag == "UnOpenHex")
+                        hit.collider.tag = "OpenHex";
+                    else if (hit.collider.tag == "HidenHex")
+                        hit.collider.tag = "OpenHex";
+                    else if (hit.collider.tag == "finish") ;
+                    else if (hit.collider.tag == "UpHex") ;
+
+                }
+            }
+        }
+    }
     void CreateHexTileMap()
     {
         float maxXMin = -mapWiedth / 2;
@@ -42,22 +72,32 @@ public class HexGridGenerator : MonoBehaviour
                     pos = new Vector3(x * titleXOffset+ titleXOffset/2, y * titleYOffset,0);
                 }
                 StartCoroutine (SetTileInfo(TempGO, x, y, pos));
+                tiles.Add(TempGO);
             }
-
         }
+        Debug.Log(tiles.Count);
     }
 
     IEnumerator SetTileInfo(GameObject tempGO, float x, float y, Vector3 pos)
     {
-        yield return new WaitForSeconds(0.00001f);
+        yield return new WaitForSeconds(0.1f);
         tempGO.transform.parent = holder;
         tempGO.name = x.ToString()+" , "+y.ToString();
         tempGO.transform.position = pos;
 
 
     }
-    void OnTriggerExit(Collider other)
+
+    public int RandomNumberGenerator()
+    {
+        int listLenght = tiles.Count;
+        var result = UnityEngine.Random.Range(0, listLenght);
+        return result;
+
+    }
+
+    /*void OnTriggerExit(Collider other)
     {
         Destroy(other.gameObject);
-    }
+    }*/
 }
---
+++ Assets/Scripts/HexGrid/HexType.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HexType : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //SceneManager.GetActiveScene();
+    }
+
+    void enemyHex()
+    {
+
+    }
+
+}
